806909e3c30ed150a3e78c2c15e846f8009af9b3
Kalai-Chezhiyan <soulreaoerx007@gmail.com>
2025-08-16 17:44:10 +0530

    implement basic setup for church website

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import Home from "./screens/home/home";
+
+function App() {
+  return <Home />;
+}
+
+export default App;
---
+++ src/components/header/header.jsx
@@ -0,0 +1,21 @@
+export default function Header() {
+  return (
+    <div className="p-[24px]">
+      <div className="flex justify-between">
+        <div className="flex">
+          <div className="pr-[10px]">Demos</div>
+          <div className="pr-[10px]">OurChurch</div>
+          <div className="pr-[10px]">Donations</div>
+        </div>
+        <div>
+          <div>RELIGIOUS</div>
+        </div>
+        <div className="flex">
+          <div className="pr-[10px]">Events</div>
+          <div className="pr-[10px]">All Pages</div>
+          <div className="pr-[10px]">Contact Us</div>
+        </div>
+      </div>
+    </div>
+  );
+}
