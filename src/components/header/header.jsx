fcb3577bbac418147b7a809ac7a342af0d57e466
Kalai-Chezhiyan <soulreaoerx007@gmail.com>
2025-08-21 23:26:24 +0530

    add content and button in home screen and refacter code in header component

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,19 +1,22 @@
 export default function Header() {
+  const leftValues = ["Demos", "Our Church", "Donations"];
+  const rightValues = ["Events", "All Pages", "Contact Us"];
+
   return (
-    <div className="p-[24px]">
+    <div className="w-full p-[24px] absolute z-1">
       <div className="flex justify-between">
         <div className="flex">
-          <div className="pr-[10px]">Demos</div>
-          <div className="pr-[10px]">OurChurch</div>
-          <div className="pr-[10px]">Donations</div>
+          {leftValues.map((value) => (
+            <div className="pr-[20px]">{value}</div>
+          ))}
         </div>
         <div>
           <div>RELIGIOUS</div>
         </div>
         <div className="flex">
-          <div className="pr-[10px]">Events</div>
-          <div className="pr-[10px]">All Pages</div>
-          <div className="pr-[10px]">Contact Us</div>
+          {rightValues.map((value) => (
+            <div className="pr-[10px]">{value}</div>
+          ))}
         </div>
       </div>
     </div>
--- src/screens/home/home.jsx
+++ src/screens/home/home.jsx
@@ -2,8 +2,38 @@ import Header from "../../components/header/header";
 
 export default function Home() {
   return (
-    <div>
+    <div className="h-full w-full">
       <Header />
+      <div className="h-full w-full absolute top-[0] ">
+        <video
+          className="h-full w-full object-cover rounded-4xl"
+          src="/assets/images/landingVedio.mp4 "
+          type="video/mp4"
+          autoPlay
+          loop
+          muted
+          controls={false}
+        />
+        <div className="w-full flex justify-center absolute top-[40%]">
+          <div>
+            <div className="pl-[30px]">
+              <div className="text-[70px]">Transform Life </div>
+              <div className="text-[70px]">Restore Hope</div>
+            </div>
+            <div className="text-[18px] text-white p-[20px]">
+              Our Coummunity Extends Beyond Physical Boundries
+            </div>
+            <div className=" text-[20px] pl-[70px]">
+              <button className="px-[10px] bg-white rounded-sm ">
+                JOIN NOW
+              </button>
+              <button className="px-[10px]  ml-[30px] border border-white border-solid text-white rounded-sm">
+                MAKE A DONATION
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
